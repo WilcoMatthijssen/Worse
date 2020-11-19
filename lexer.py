a163500e296d677d514bfb100d9ac1314af9086b
Wilco <wilco@matthijssen.xyz>
2020-11-19 15:43:39 +0100

    Update lexer.py
    Changed lexer to use regex

--- lexer.py
+++ lexer.py
@@ -1,101 +1,75 @@
+import re
 from enum import Enum
 from typing import Callable
+from typing import List
 
 
-class TokenType(Enum):
-    EOF = 1
-    INT = 2
-    NONE = 3
-    IF = 4
-    ID = 5
-    WHILE = 6
-    OP = 7
-    DEF = 8
-    SEP =9
-    END = 10
-    PRINT = 11
-    ASSIGN = 12
-    OPENBR =13
-    CLOSEBR = 14
+class Tokens(Enum):
+    WHILE   = "(?<!\w)while(?!\w)"
+    IF      = "(?<!\w)if(?!\w)"
+    PRINT   = "(?<!\w)print(?!\w)"
+    ASSIGN  = "(?<![=:])\=(?![=:])"
+    DEF     = "\?"
+    END     = "\;"
+    SEP     = "\,"
+    EQUALS  = "\=\="
+    NOTEQUAL= "\!\="
+    GREATER = "\:\="
+    LESSER  = "\=\:"
+    ADD     = "\+"
+    SUB     = "\-"
+    OPENBR  = "\("
+    CLOSEBR = "\)"
+    ID      = "[a-zA-Z]\w*"
+    DIGIT   = "[0-9]+"
+
+
+class LexerError(Exception):
+    def __init__(self, char: str, pos: int):
+        """ Error for an unknown char encountered whilst lexing. """
+        self.char = char
+        self.pos = pos
+        super().__init__(f"unknown char \"{self.char}\" found at pos {self.pos}")
 
 
 class Token:
-    def __init__(self):
-        self.type: TokenType = TokenType.NONE
-        self.value: str = ""
+    def __init__(self, kind: Tokens, content: str, pos: int):
+        """ Creates a Token containing content, pos and kind. """
+        self.content = content
+        self.kind = kind
+        self.pos = pos
 
-    def __str__(self):
-        return f"\n({self.type}  \"{self.value}\")"
+    def __str__(self) -> str:
+        """ returns content, pos and kind of Token. """
+        return f"(<{self.content}> at pos {self.pos} is {self.kind})"
 
-    def __repr__(self):
-        return f"\n({self.type}  \"{self.value}\")"
-
-
-def Lexer(code: str) -> (Token, str):
-    def identify(value):
-        keywords = {
-            "?": TokenType.DEF,
-            ".": TokenType.END,
-            ",": TokenType.SEP,
-            "+": TokenType.OP,
-            "-": TokenType.OP,
-            "=": TokenType.ASSIGN,
-            "(": TokenType.OPENBR,
-            ")": TokenType.CLOSEBR,
-            "if": TokenType.IF,
-            "while": TokenType.WHILE,
-            "print": TokenType.PRINT,
-        }
-        type = TokenType.NONE
-        if value in keywords:
-            type = keywords[value]
-        elif value[0].isalpha():
-            type = TokenType.ID
-        elif value[0].isdigit():
-            type = TokenType.INT
-        return type
-
-    def aCheck(t: Token, code: str, check: Callable) -> (Token, str):
-        if len(code) > 0:
-            if check(code[0]):
-                t.value += code[0]
-                code = code[1:]
-                return aCheck(t, code, check)
-        t.type = identify(t.value)
-        return t, code
-
-    def getNextKey(t: Token, code: str) -> (Token, str):
-        if code == "" and t.value == "":
-            t.type = TokenType.EOF
-            return t, code
-        t.value = code[0]
-        code = code[1:]
-        if t.value.isdigit():
-            return aCheck(t, code, lambda c: c.isdigit())
-
-        elif t.value.isalpha():
-            return aCheck(t, code, lambda c: c.isdigit() or c.isalpha())
-
-        elif t.value in "?+-/.,=()":
-            t.type = identify(t.value)
-            return t, code
-
-        else:
-            t.value = ""
-            return getNextKey(t, code)
+    def __repr__(self) -> str:
+        """ returns content, pos and kind of Token. """
+        return f"(<{self.content}> at pos {self.pos} is {self.kind})"
 
 
-    t, code = getNextKey(Token(), code)
-    if len(code) > 0:
-        return [t] + Lexer(code)
-    return [t]
+def lexer(code: str, rules: Enum) -> List[Token]:
+    def get_tokens(string: str, match_func: Callable[[str, int], re.Match],
+                   skip_func: Callable[[str, int], re.Match], pos: int = 0) -> List[Token]:
+        """ Recursively gets all keys present in string. """
+        if (pos := skip.end() if (skip := skip_func(string, pos)) else pos) >= len(string):
+            return []
+        if m := match_func(code, pos):
+            match = Token(m.lastgroup, m.group(m.lastgroup),  m.start())
+            return [match] + get_tokens(code, match_func, skip_func, m.end())
+        raise LexerError(string[pos], pos)
+
+    find_whitespace = re.compile(r"[\s]+").match
+    joined_rules = "|".join([f"(?P<{rule.name}>{rule.value})" for rule in rules])
+    regex = re.compile(joined_rules).match
+    return get_tokens(code, regex, find_whitespace)
+
 
 if __name__ == "__main__":
     file = open("Worse.txt")
-    code = file.read()
+    filecontent = file.read()
     file.close()
-
-    print(Lexer(code))
-
+    for e in lexer(filecontent, Tokens):
+        print(e)
 
 
