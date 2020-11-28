1cb289b16b34f38f9a1b1785267a16bf509d999c
Wilco <wilco@matthijssen.xyz>
2020-11-28 22:57:58 +0100

    Simplified parser by reducing if statements

--- parser.py
+++ parser.py
@@ -1,5 +1,5 @@
 from lexer import *
-import enum
+from enum import Enum
 
 
 class ParserError(Exception):
@@ -7,97 +7,78 @@ class ParserError(Exception):
         super().__init__(msg)
 
 
-class AST(enum.Enum):
-    OPERATION = 1
-    VALUE = 2
-    FUNCDEF = 3
-    FUNCEXE = 4
-    IFWHILE = 5
-    PRINT = 6
-    VAR = 7
-    IDLE = 8
-    ASSIGN = 9
-
-
 class Node:
-    def __init__(self, species):
-        self.species = species
+    def __init__(self):
+        pass
 
 
 class Assign(Node):
     def __init__(self, variable, value):
-        Node.__init__(self, AST.ASSIGN)
+        Node.__init__(self)
         self.variable = variable
         self.value = value
 
     def __str__(self) -> str:
-        """ returns content, pos and kind of Token. """
         return f"{self.variable.content} = {self.value}"
 
     def __repr__(self) -> str:
-        """ returns content, pos and kind of Token. """
         return self.__str__()
 
 
 class Value(Node):
     def __init__(self, value):
-        Node.__init__(self, AST.VALUE)
+        Node.__init__(self)
         self.value = value
 
     def __str__(self) -> str:
-        """ returns content, pos and kind of Token. """
         return f"{self.value.content}"
 
     def __repr__(self) -> str:
-        """ returns content, pos and kind of Token. """
         return self.__str__()
 
 
 class Variable(Node):
     def __init__(self, name):
-        Node.__init__(self, AST.VAR)
+        Node.__init__(self)
         self.name = name
+
     def __str__(self) -> str:
-        """ returns content, pos and kind of Token. """
         return self.__repr__()
 
     def __repr__(self) -> str:
-        """ returns content, pos and kind of Token. """
         return f"{self.name.content}"
+
 
 class FuncExe(Node):
     def __init__(self, name, params):
-        Node.__init__(self, AST.FUNCEXE)
+        Node.__init__(self)
         self.name = name
         self.params: List = params
 
     def __str__(self) -> str:
-        """ returns content, pos and kind of Token. """
         return self.__repr__()
 
     def __repr__(self) -> str:
-        """ returns content, pos and kind of Token. """
         return f"{self.name.content} {self.params}"
+
 
 class FuncDef(Node):
     def __init__(self, name, params, code):
-        Node.__init__(self, AST.FUNCDEF)
+        Node.__init__(self)
         self.name = name
         self.params = params
         self.code = code
 
     def __str__(self) -> str:
-        """ returns content, pos and kind of Token. """
         return f"Define {self.name.content}({self.params}){self.code};"
 
     def __repr__(self) -> str:
-        """ returns content, pos and kind of Token. """
         return self.__str__()
 
 
 class Print(Node):
     def __init__(self, value):
-        Node.__init__(self, AST.PRINT)
+        Node.__init__(self)
         self.value = value
 
     def __str__(self) -> str:
@@ -111,7 +92,7 @@ class Print(Node):
 
 class IfWhile(Node):
     def __init__(self, expression, code, is_while=False):
-        Node.__init__(self, AST.PRINT)
+        Node.__init__(self)
         self.expression = expression
         self.code = code
         self.is_while = is_while
@@ -127,8 +108,8 @@ class IfWhile(Node):
 
 
 class Operation(Node):
-    def __init__(self,lhs, operator, rhs):
-        Node.__init__(self, AST.OPERATION)
+    def __init__(self, lhs, operator, rhs):
+        Node.__init__(self)
         self.operator = operator
         self.rhs = rhs
         self.lhs = lhs
@@ -141,72 +122,77 @@ class Operation(Node):
         """ returns content, pos and kind of Token. """
         return self.__str__()
 
+
 # ---------------------------------- ## ---------------------------------- ## ---------------------------------- #
-# ---------------------------------- ## ---------------------------------- ## ---------------------------------- #
-# ---------------------------------- ## ---------------------------------- ## ---------------------------------- #
-# ---------------------------------- ## ---------------------------------- ## ---------------------------------- #
-# ---------------------------------- ## ---------------------------------- ## ---------------------------------- #
-
-
-def parser(tokens):
-    nodes, res = p(tokens)
-    print("RESTANT", res)
-    return nodes
-
-
-def p(tokens):
-    node = None
-
-    if peek(tokens, [TokenSpecies.ID]):
-        val = tokens.pop(0)
-        if peek(tokens, [TokenSpecies.ASSIGN]):
-            tokens.pop(0)
-            pars, tokens = value(tokens)
-            node = Assign(val, pars)
-
-    if peek(tokens, [TokenSpecies.PRINT]):
-        tokens.pop(0)
-        if peek(tokens, [TokenSpecies.OPENBR]):
-            tokens.pop(0)
-            pars, tokens = params(tokens)
-            node = Print(pars)
-
-    if peek(tokens, [TokenSpecies.DEF]):
-        tokens.pop(0)
-        if peek(tokens, [TokenSpecies.ID]):
-            id = tokens.pop(0)
-            if peek(tokens, [TokenSpecies.OPENBR]):
-                tokens.pop(0)
-                pars, tokens = defparams(tokens)
-                cod, tokens = p(tokens)
-                node = FuncDef(id, pars, cod)
-
-    if peek(tokens, [TokenSpecies.WHILE, TokenSpecies.IF]):
-        is_while = peek(tokens, [TokenSpecies.WHILE])
-        tokens.pop(0)
-        if peek(tokens, [TokenSpecies.OPENBR]):
-            tokens.pop(0)
-            pars, tokens = value(tokens)
-            if peek(tokens, [TokenSpecies.CLOSEBR]):
-                tokens.pop(0)
-                cod, tokens = p(tokens)
-                node = IfWhile(pars, cod, is_while)
-
-    # ---------------------------------- #
-
-    if node:
-        if peek(tokens, [TokenSpecies.END]):
-            tokens.pop(0)
-            other_nodes, tokens = p(tokens)
-            return [node] + other_nodes, tokens
-        else:
-            raise ParserError("NO END CLAUSE")
-
-    return [], tokens
-
 
 def peek(tokens, species):
     return len(tokens) != 0 and tokens[0].species in species
+
+
+def get_def_parameters(tokens):
+    params = [Variable(tokens.pop(0))] if peek(tokens, [TokenSpecies.ID]) else []
+    if peek(tokens, [TokenSpecies.SEP]):
+        other_params, tokens = get_def_parameters(tokens[1:])
+        return params + other_params, tokens
+    elif peek(tokens, [TokenSpecies.CLOSEBR]):
+        return params, tokens
+    else:
+        raise ParserError("EXPECTED PARAMS U FUCKING DOG")
+
+
+def get_or_riot(tokens, species):
+    if len(tokens) != 0:
+        if (token := tokens.pop(0)).species == species:
+            return token, tokens
+        else:
+            raise ParserError(f"EXPECTED {species} BUT GOT {token}")
+    else:
+        raise ParserError(f"EXPECTED {species} BUT GOT NOTHING")
+
+
+def parser(tokens):
+    _, tokens = get_or_riot(tokens, TokenSpecies.DEF)
+    name, tokens = get_or_riot(tokens, TokenSpecies.ID)
+    _, tokens = get_or_riot(tokens, TokenSpecies.OPENBR)
+    param, tokens = get_def_parameters(tokens)
+    _, tokens = get_or_riot(tokens, TokenSpecies.CLOSEBR)
+    instr, tokens = get_instructions(tokens)
+    _, tokens = get_or_riot(tokens, TokenSpecies.END)
+
+    function = [FuncDef(name, param, instr)]
+    return function + parser(tokens) if len(tokens) != 0 else function
+
+
+def get_instructions(tokens):
+    node = None
+    if peek(tokens, [TokenSpecies.ID]):
+        val = tokens.pop(0)
+        _, tokens = get_or_riot(tokens, TokenSpecies.ASSIGN)
+        pars, tokens = value(tokens)
+        node = Assign(val, pars)
+
+    elif peek(tokens, [TokenSpecies.PRINT]):
+        tokens.pop(0)
+        _, tokens = get_or_riot(tokens, TokenSpecies.OPENBR)
+
+        pars, tokens = params(tokens)
+        node = Print(pars)
+
+    elif peek(tokens, [TokenSpecies.WHILE, TokenSpecies.IF]):
+        is_while = TokenSpecies.WHILE == tokens.pop(0).species
+
+        _, tokens = get_or_riot(tokens, TokenSpecies.OPENBR)
+        pars, tokens = value(tokens)
+        _, tokens = get_or_riot(tokens, TokenSpecies.CLOSEBR)
+
+        cod, tokens = get_instructions(tokens)
+        node = IfWhile(pars, cod, is_while)
+
+    if node:
+        _, tokens = get_or_riot(tokens, TokenSpecies.END)
+        other_nodes, tokens = get_instructions(tokens)
+        return [node] + other_nodes, tokens
+    return [], tokens
 
 
 def params(tokens):
@@ -219,20 +205,6 @@ def params(tokens):
     if peek(tokens, [TokenSpecies.CLOSEBR]):
         tokens = tokens[1:]
         return [val], tokens
-
-    raise ParserError("Params not ended properly")
-
-
-def defparams(tokens):
-    if peek(tokens, [TokenSpecies.ID]):
-        id = Variable(tokens.pop(0))
-        if peek(tokens, [TokenSpecies.SEP]):
-            tokens.pop(0)
-            other_ids, tokens = defparams(tokens)
-            return [id]+other_ids, tokens
-        elif peek(tokens, [TokenSpecies.CLOSEBR]):
-            tokens.pop(0)
-            return [id], tokens
 
     raise ParserError("Params not ended properly")
 
@@ -251,14 +223,18 @@ def math(lhs, tokens):
     if rhs:
         if rhs.species == TokenSpecies.ID and peek(tokens, [TokenSpecies.OPENBR]):
             tokens.pop(0)
-            pars, tokens = params(tokens)
-            rhs = FuncExe(rhs, pars)
+            if peek(tokens, [TokenSpecies.CLOSEBR]):
+                tokens.pop(0)
+                rhs = FuncExe(rhs, [])
+            else:
+                pars, tokens = params(tokens)
+                rhs = FuncExe(rhs, pars)
         else:
             rhs = Value(rhs)
 
         if peek(tokens, [TokenSpecies.ADD, TokenSpecies.SUB,
                          TokenSpecies.GREATER, TokenSpecies.EQUALS, TokenSpecies.LESSER]):
-            lhs, tokens = math(lhs,tokens)
+            lhs, tokens = math(lhs, tokens)
             node = Operation(lhs, oper, rhs)
         elif peek(tokens, [TokenSpecies.SEP, TokenSpecies.CLOSEBR, TokenSpecies.END]):
             node = Operation(lhs, oper, rhs)
@@ -271,31 +247,24 @@ def math(lhs, tokens):
 
 
 def value(tokens):
-    # val = None
-    # if peek(tokens, [TokenSpecies.DIGIT]):
-    #     val = tokens.pop(0)
-    # elif peek(tokens, [TokenSpecies.ID]):
-    #     val = tokens.pop(0)
     val = tokens.pop(0) if peek(tokens, [TokenSpecies.DIGIT, TokenSpecies.ID]) else None
-    # ---------------------------------- #
 
     if val:
-        # maak func
         if val.species == TokenSpecies.ID and peek(tokens, [TokenSpecies.OPENBR]):
             tokens.pop(0)
-            pars, tokens = params(tokens)
-            val = FuncExe(val, pars)
-
-        # maak waarde
+            if peek(tokens, [TokenSpecies.CLOSEBR]):
+                tokens.pop(0)
+                val = FuncExe(val, [])
+            else:
+                pars, tokens = params(tokens)
+                val = FuncExe(val, pars)
         else:
             val = Value(val)
 
-        # maak som
         if peek(tokens, [TokenSpecies.ADD, TokenSpecies.SUB,
                          TokenSpecies.GREATER, TokenSpecies.EQUALS, TokenSpecies.LESSER]):
             val, tokens = math(val, tokens)
 
-        # check end
         if peek(tokens, [TokenSpecies.SEP, TokenSpecies.CLOSEBR, TokenSpecies.END]):
             return val, tokens
         else:
@@ -305,8 +274,12 @@ def value(tokens):
 
 
 if __name__ == "__main__":
+    file = open("Worse.txt")
+    filecontent = file.read()
+    file.close()
     "if(a)a=11;print(a1,aa,a,a,a,a,a);;"
-    tok = lexer("?fu(a)a=1;while(a)a=12+10+help(a)+20;;;", TokenSpecies, r"[^\:\=\!\+\-\(\)\,\;\?\s\w]")
-    #print(tok)
-    print("RESULTAAT", parser(tok))
-
+    "?fu(a)a=1;while(a)a=12+10+help(a)+20;;;"
+    tok = lexer(filecontent, TokenSpecies, r"[^\:\=\!\+\-\(\)\,\;\?\s\w]")
+    result = parser(tok)
+    for r in result:
+        print(r)
