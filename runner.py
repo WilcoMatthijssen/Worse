f30b5abf7f301021e2c2c3a1449d7e12b17cd922
Wilco <wilco@matthijssen.xyz>
2021-02-17 21:15:34 +0100

    Written without HU restrictions

--- runner.py
+++ runner.py
@@ -2,9 +2,6 @@ from lexer import lexer
 from parse import parser
 from classes import *
 import operator
-from functools import reduce
-import sys
-sys.setrecursionlimit(10000)
 
 
 class RunnerError(Exception):
@@ -13,107 +10,123 @@ class RunnerError(Exception):
         super().__init__(msg)
 
 
-# get_op_func :: Type[Enum] -> Callable
-@deepcopy_decorator
-def get_op_func(species: Type[Enum]) -> Callable:
-    """ Give corresponding function to enum"""
-    op = {TokenSpecies.ADD: operator.add,
-          TokenSpecies.SUB: operator.sub,
-          TokenSpecies.GREATER: operator.gt,
-          TokenSpecies.LESSER: operator.lt,
-          TokenSpecies.EQUALS: operator.eq,
-          TokenSpecies.NOTEQUAL: operator.ne,
-          TokenSpecies.MUL: operator.mul,
-          TokenSpecies.DIV: operator.floordiv}
-    return op[species]
+def execute_function_by_name(function_name, parameter_values, functions):
+    assert function_name in functions, f"Function \"{function_name}\" does not exist."
+
+    function = functions.get(function_name)
+    variables = dict(zip(function.params, parameter_values))
+    for sub_action in function.actions:
+        variables = execute_action(sub_action, variables, functions)
+
+    if "sos" not in variables:
+        RunnerError(f"Function {function_name} doesn't return a value.")
+    return variables.get("sos")
 
 
-# get_value :: Type[ValueNode] -> Dict[str, int] -> Dict[str, FuncDefNode] -> int
-@deepcopy_decorator
-def get_value(action: Type[ValueNode], variables: Dict[str, int], functions: Dict[str, FuncDefNode]) -> int:
-    """ Gets integer value of given action. """
-    value = 0
-    if isinstance(action, OperationNode):
-        lhs = get_value(action.lhs, variables, functions)
-        rhs = get_value(action.rhs, variables, functions)
-        value = int(get_op_func(action.operator)(lhs, rhs))
+def execute_action(action, variables, functions):
+    if isinstance(action, AssignNode):
+        return execute_assign(action, variables, functions)
 
-    elif isinstance(action, VariableNode):
-        if action.name not in variables.keys():
-            raise RunnerError(f"Variable \"{action.name}\" at character {action.pos} not defined.")
-        value = variables[action.name]
+    elif isinstance(action, PrintNode):
+        return execute_print(action, variables, functions)
 
-    elif isinstance(action, IntNode):
-        value = int(action.value)
+    elif isinstance(action, IfWhileNode):
+        return execute_if_or_while(action, variables, functions)
 
-    elif isinstance(action, FuncExeNode):
-        if action.name not in functions.keys():
-            raise RunnerError(f"Function \"{action.name}\" at character {action.pos} does not exist.")
-        param_values = list(map(lambda val: get_value(val, variables, functions), action.params))
-        if len(param_values) != len(functions[action.name].params.keys()):
-            raise RunnerError(f"Params for \"{action.name}\" given at character {action.pos} dont match function.")
-
-        parameters = dict(zip(functions[action.name].params.keys(), param_values))
-        value = run_function(action.name, parameters, functions)
-
-    return value
+    else:
+        raise RunnerError(f"Action at {action.pos} of type {type(action)} isn't supported")
 
 
-# run_ifwhile :: IfWhileNode -> Dict[str, int] -> Dict[str, FuncDefNode] -> Dict[str, int]
-@deepcopy_decorator
-def run_ifwhile(loop: IfWhileNode, variables: Dict[str, int], functions: Dict[str, FuncDefNode]) -> Dict[str, int]:
-    """ Run if or while loop recursively. """
-    if get_value(loop.condition, variables, functions) != 0:
-        new_vars = reduce(lambda var, action: run_action(var, functions, action), loop.actions, variables)
-        if loop.is_while:
-            return run_ifwhile(loop, new_vars, functions)
-        return new_vars
+def execute_assign(action, variables, functions):
+    value = retrieve_value(action.value, variables, functions)
+    variables[action.name] = value
     return variables
 
 
-# run_action :: Dict[str, int] -> Dict[str, FuncDefNode] -> Type[ActionNode] -> Dict[str, int]
-@deepcopy_decorator
-def run_action(var: Dict[str, int], funcs: Dict[str, FuncDefNode], action: Type[ActionNode]) -> Dict[str, int]:
-    """ Runs action and returns updated variables and unchanged functions for the use of reduce. """
-
-    if isinstance(action, AssignNode):
-        new_vars = {**var, **{action.name: get_value(action.value, var, funcs)}}
-    elif isinstance(action, PrintNode):
-        print("".join(map(lambda val: chr(get_value(val, var, funcs)), action.value)))
-        new_vars = var
-    else:  # isinstance(action, IfWhileNode):
-        new_vars = run_ifwhile(action, var, funcs)
-    return new_vars
+def execute_print(action, variables, functions):
+    values = [retrieve_value(value_node, variables, functions) for value_node in action.value]
+    values_as_char = map(chr, values)
+    print(*values_as_char)
+    return variables
 
 
-# run_function ::  str -> Dict[str, int] ->  Dict[str, FuncDefNode] -> int
-@deepcopy_decorator
-def run_function(func_name: str, variables: Dict[str, int], functions: Dict[str, FuncDefNode]) -> int:
-    """ Execute function and return the "sos" variable. """
-    # Check is func exists.
-    if func_name not in functions.keys():
-        raise RunnerError(f"Function \"{func_name}\" not defined")
+def execute_if_or_while(action, variables, functions):
+    while True:
+        # Prevent from running code when condition evaluates as 0.
+        if retrieve_value(action.condition, variables, functions) == 0:
+            break
 
-    # Run function
-    new_vars = reduce(lambda var, action: run_action(var, functions, action), functions[func_name].actions, variables)
+        for sub_action in action.actions:
+            variables = execute_action(sub_action, variables, functions)
 
-    # Check if function returns value.
-    if "sos" in new_vars.keys():
-        return new_vars["sos"]
-    raise RunnerError(f"Function \"{func_name}\" at character {functions[func_name].pos} doesn't assign a value to sos")
+        # Prevent from running multiple times when given action is not a while loop.
+        if action.is_while == 0:
+            break
+    return variables
 
 
-# runner ::  List[FuncDefNode] -> str -> None
-@deepcopy_decorator
-def runner(ast: List[FuncDefNode], start_func_name: str = "main") -> None:
-    """ Runs the ast by executing the start_func_name. """
+def retrieve_value(action, variables, functions):
+    if isinstance(action, OperationNode):
+        return execute_operation(action, variables, functions)
 
-    functions = dict(map(lambda func: (func.name, func), ast))
-    print(f"Process starting with function: {start_func_name}")
-    result = run_function(start_func_name, {}, functions)
-    print(f"Process finished with {result}")
-    return result
+    elif isinstance(action, VariableNode):
+        return retrieve_value_from_variable(action, variables, functions)
 
+    elif isinstance(action, IntNode):
+        return retrieve_value_from_int(action, variables, functions)
+
+    elif isinstance(action, FuncExeNode):
+        return execute_function(action, variables, functions)
+
+    else:
+        raise RunnerError(f"Action at {action.pos} of type {type(action)} isn't supported")
+
+
+def get_operator_by_name(operator_name):
+    operators = {
+        TokenSpecies.ADD: operator.add,
+        TokenSpecies.SUB: operator.sub,
+        TokenSpecies.GREATER: operator.gt,
+        TokenSpecies.LESSER: operator.lt,
+        TokenSpecies.EQUALS: operator.eq,
+        TokenSpecies.NOTEQUAL: operator.ne,
+        TokenSpecies.MUL: operator.mul,
+        TokenSpecies.DIV: operator.floordiv
+    }
+    if operator_name not in operators:
+        RunnerError(f"Operator {operator_name} doesn't exist.")
+    return operators[operator_name]
+
+
+def execute_operation(action, variables, functions):
+    lhs = retrieve_value(action.lhs, variables, functions)
+    rhs = retrieve_value(action.rhs, variables, functions)
+    operator_func = get_operator_by_name(action.operator)
+
+    return int(operator_func(lhs, rhs))
+
+
+def retrieve_value_from_variable(action, variables, functions):
+    if action.name not in variables:
+        raise RunnerError(f"Variable {action} at {action.pos} doesn't exist.")
+    return variables.get(action.name)
+
+
+def retrieve_value_from_int(action, variables, functions):
+    try:
+        return int(action.value)
+    except ValueError:
+        print("Can't convert value to int.")
+
+
+def execute_function(action, variables, functions):
+    parameters = [retrieve_value(value, variables, functions) for value in action.params]
+    return execute_function_by_name(action.name, parameters, functions)
+
+
+def runner(function_list, begin_func_name="main"):
+    functions = {func.name: func for func in function_list}
+    print(execute_function_by_name(begin_func_name, (), functions))
 
 
 if __name__ == "__main__":
@@ -123,7 +136,7 @@ if __name__ == "__main__":
 
     tokenized_code = lexer(file_content)
     ast = parser(tokenized_code)
-    runner(ast)
+    runner(ast, "main")
 
 
 
