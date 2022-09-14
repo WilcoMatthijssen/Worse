5216b50d8c1e91b5d5cfa0e691c3859c250aa94d
Wilco <wilco@matthijssen.xyz>
2022-09-14 20:00:12 +0200

    Changed runner.py to use match case

--- runner.py
+++ runner.py
@@ -5,21 +5,15 @@ import sys
 from lexer import lexer
 from parse import parser
 import classes as cl
+from dataclasses import dataclass
+sys.setrecursionlimit(700)
 
 
-sys.setrecursionlimit(1200)
 
 
 class RunnerError(Exception):
     """ Error for when something goes wrong running the Worse code """
 
-    def __init__(self, msg: str):
-        super().__init__(msg)
-
-
-# ------------------------------------------- #
-#              Action interpreting            #
-# ------------------------------------------- #
 
 
 def execute_function_by_name(function_name: str, parameter_values: List[int], functions: Dict[str, cl.FuncDefNode]):
@@ -37,30 +31,24 @@ def execute_function_by_name(function_name: str, parameter_values: List[int], fu
     return variables.get("sos")
 
 
-def dict_executor(key, dictionary, parameters, err):
-    if key not in dictionary:
-        raise err
-    return dictionary[key](*parameters)
-
-
 def execute_action(action: Type[cl.ActionNode], variables: Dict[str, int], functions: Dict[str, cl.FuncDefNode]):
     """execute_action :: Type[ActionNode] -> Dict[str, int] -> Dict[str, FuncDefNode] -> int"""
-    options = {
-        cl.AssignNode: execute_assign,
-        cl.PrintNode: execute_print,
-        cl.IfWhileNode: execute_if_or_while
-    }
-    err = RunnerError(
-        f"Action at {action.pos} of type {type(action)} isn't supported")
-    return dict_executor(type(action), options, (action, variables, functions), err)
-
+    match action:
+        case cl.AssignNode():
+            return execute_assign(action, variables, functions)
+        case cl.PrintNode():
+            return execute_print(action, variables, functions)
+        case cl.IfWhileNode():
+            return execute_if_or_while(action, variables, functions)
+        case _:
+            RunnerError(f"Action at {action.pos} of type {type(action)} isn't supported")
+    return None, None # Can't reach this but Pylint will go crazy without
 
 
 def execute_assign(action: cl.AssignNode, variables: Dict[str, int], functions: Dict[str, cl.FuncDefNode]):
     """execute_assign :: AssignNode -> Dict[str, int] -> Dict[str, FuncDefNode] -> int"""
     variables[action.name] = retrieve_value(action.value, variables, functions)
     return variables
-
 
 
 def execute_print(action: cl.PrintNode, variables: Dict[str, int], functions: Dict[str, cl.FuncDefNode]):
@@ -70,7 +58,6 @@ def execute_print(action: cl.PrintNode, variables: Dict[str, int], functions: Di
         action.value)
     print(*values_as_char)
     return variables
-
 
 
 def execute_if_or_while(action: cl.IfWhileNode, variables: Dict[str, int], functions: Dict[str, cl.FuncDefNode]):
@@ -83,86 +70,49 @@ def execute_if_or_while(action: cl.IfWhileNode, variables: Dict[str, int], funct
     return variables
 
 
-# -------------------------------------------------- #
-#                  Value interpreting                #
-# -------------------------------------------------- #
-
-
-
-def retrieve_value(action: Type[cl.ValueNode], variables: Dict[str, int], functions: Dict[str, cl.FuncDefNode]):
+def retrieve_value(action: Type[cl.ValueNode], variables: Dict[str, int], functions: Dict[str, cl.FuncDefNode]) -> int:
     """retrieve_value :: Type[ValueNode] -> Dict[str, int] -> Dict[str, FuncDefNode] -> int"""
-    options = {
-        cl.OperationNode: execute_operation,
-        cl.VariableNode: retrieve_value_from_variable,
-        cl.IntNode: retrieve_value_from_int,
-        cl.FuncExeNode: execute_function
-    }
-    err = RunnerError(
-        f"Action at {action.pos} of type {type(action)} isn't supported")
-    return dict_executor(type(action), options, (action, variables, functions), err)
+    match action:
+        case cl.OperationNode():
+            return execute_operation(action, variables, functions)
+        case cl.VariableNode():
+            if action.name in variables:
+                return variables.get(action.name)
+            raise RunnerError(f"Variable {action} at {action.pos} doesn't exist.")
+        case cl.IntNode():
+            return int(action.value)
+        case cl.FuncExeNode():
+            parameters = (retrieve_value(value, variables, functions) for value in action.params)
+            return execute_function_by_name(action.name, parameters, functions)
+        case _:
+            raise RunnerError(f"Action at {action.pos} of type {type(action)} isn't supported")
 
 
 def execute_operation(action: cl.OperationNode, variables: Dict[str, int], functions: Dict[str, cl.FuncDefNode]):
     """execute_operation :: OperationNode -> Dict[str, int] -> Dict[str, FuncDefNode] -> int"""
     lhs = retrieve_value(action.lhs, variables, functions)
     rhs = retrieve_value(action.rhs, variables, functions)
-    operators = {
-        cl.TokenSpecies.ADD: operator.add,
-        cl.TokenSpecies.SUB: operator.sub,
-        cl.TokenSpecies.GREATER: operator.gt,
-        cl.TokenSpecies.LESSER: operator.lt,
-        cl.TokenSpecies.EQUALS: operator.eq,
-        cl.TokenSpecies.NOTEQUAL: operator.ne,
-        cl.TokenSpecies.MUL: operator.mul,
-        cl.TokenSpecies.DIV: operator.floordiv
-    }
     match action.operator:
         case cl.TokenSpecies.ADD:
-            oper = operator.add
+            return operator.add(lhs, rhs)
         case cl.TokenSpecies.SUB:
-            oper = operator.sub
+            return operator.sub(lhs, rhs)
         case cl.TokenSpecies.GREATER:
-            oper = operator.gt
+            return operator.gt(lhs, rhs)
         case cl.TokenSpecies.LESSER:
-            oper = operator.lt
+            return operator.lt(lhs, rhs)
         case cl.TokenSpecies.EQUALS:
-            oper = operator.eq
+            return operator.eq(lhs, rhs)
         case cl.TokenSpecies.NOTEQUAL:
-            oper = operator.ne
+            return operator.ne(lhs, rhs)
         case cl.TokenSpecies.MUL:
-            oper = operator.mul
+            operator.mul(lhs, rhs)
         case cl.TokenSpecies.DIV:
-            oper = operator.floordiv
+            return operator.floordiv(lhs, rhs)
         case _:
             raise RunnerError(f"Operator {action.operator} doesn't exist.")
-    return int(oper(lhs, rhs))
 
 
-def retrieve_value_from_variable(action: cl.VariableNode, variables: Dict[str, int], functions: Dict[str, cl.FuncDefNode]):
-    """retrieve_value_from_variable :: VariableNode -> Dict[str, int] -> Dict[str, FuncDefNode] -> int"""
-    if action.name not in variables:
-        raise RunnerError(f"Variable {action} at {action.pos} doesn't exist.")
-    return variables.get(action.name)
-
-
-def retrieve_value_from_int(action: cl.IntNode, variables: Dict[str, int], functions: Dict[str, cl.FuncDefNode]):
-    """retrieve_value_from_int :: IntNode -> Dict[str, int] -> Dict[str, FuncDefNode] -> int"""
-    try:
-        return int(action.value)
-    except ValueError:
-        raise RunnerError(f"Can't interpret {action} at {action.pos} to int.")
-
-
-def execute_function(action: cl.FuncExeNode, variables: Dict[str, int], functions: Dict[str, cl.FuncDefNode]):
-    """execute_function :: FuncExeNode -> Dict[str, int] -> Dict[str, FuncDefNode] -> int"""
-    parameters = [retrieve_value(value, variables, functions)
-                  for value in action.params]
-    return execute_function_by_name(action.name, parameters, functions)
-
-
-# --------------------------------------------- #
-#              Main functions                   #
-# --------------------------------------------- #
 def runner(function_list: List[cl.FuncDefNode], begin_func_name: str = "main"):
     """runner :: List[FuncDefNode] -> str -> None"""
     functions = {func.name: func for func in function_list}
